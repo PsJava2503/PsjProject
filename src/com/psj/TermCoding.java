7fcd9ae91fba9fd8dd542df7e40d3e7f01b3773c
皮少军 <549725426@qq.com>
2021-12-01 23:12:51 +0800

    2021-12-1

--- src/com/psj/TermCoding.java
+++ src/com/psj/TermCoding.java
@@ -1,60 +1,48 @@
 package com.psj;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 public class TermCoding {
 
-    static List<String> res = new ArrayList<>();
-    static final int SEG_COUNT = 4;
-
     public static void main(String[] args) {
-        String s = "25525511135";
-        System.out.println(restoreIpAddresses(s));
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        System.out.println(pathSum(root, 3));
     }
 
-    public static List<String> restoreIpAddresses(String s) {
-        dfs(s, 0, 0, new ArrayList<>());
+    static List<List<Integer>> res = new ArrayList<>();
+
+    public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum, new ArrayList<Integer>());
         return res;
     }
 
-    public static void dfs(String s, int count, int index, List<String> sb) {
-        if (count == 3 && isValid(s.substring(index))) {
-            StringBuilder temp = new StringBuilder();
-            for (int i = 0; i < sb.size(); i++) {
-                temp.append(sb.get(i));
-            }
-            temp.append(s.substring(index));
-            res.add(temp.toString());
+    public static void dfs(TreeNode node, int restTarget, List<Integer> list) {
+        if (node == null) {
+            return;
         }
-
-        for (int i = index + 1; i < s.length(); i++) {
-            if (isValid(s.substring(index, i))) {
-                StringBuilder temp = new StringBuilder();
-                temp.append(s, index, i);
-                temp.append(".");
-                sb.add(temp.toString());
-                count++;
-                dfs(s, count, i, sb);
-                count--;
-                sb.remove(sb.size() - 1);
-            }
+        list.add(node.val);
+        restTarget -= node.val;
+        if (restTarget == 0 && node.left == null && node.right == null) {
+            res.add(new ArrayList(list));
         }
+        dfs(node.left, restTarget, list);
+        dfs(node.right, restTarget, list);
+        list.remove(list.size() - 1);
     }
+}
 
-    public static boolean isValid(String s){
-        if (s.length() == 0) return false;
-        char[] cs = s.toCharArray();
-        if (cs[0] == '0' && cs.length != 1) return false;
-        int res = 0;
-        for (int i = 0; i < cs.length; i++){
-            res = res * 10 + cs[i] - '0';
-        }
-        if (res >= 0 && res <= 255){
-            return true;
-        }
-        return false;
-    }
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
 }
